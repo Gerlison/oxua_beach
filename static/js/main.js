56df58c84f82ac73044c0d8554743ab1336fb3b7
Francisco Gerlison <franciscogerlison1@gmail.com>
2019-08-05 23:28:15 -0300

    main

--- static/js/main.js
+++ static/js/main.js
@@ -4,15 +4,24 @@ $(document).ready(function() {
     .append(
       `
       <div>
-        <p class="slider-caption">Donec sollicitudin molestie malesuada.</p>
+        <div class="caption">
+          <p>O verão chegou com novidades</p>
+          <a href="#">VER PRODUTOS</a>
+        </div>
         <img class="img-fluid" src="./assets/images/image.png" alt="" />
       </div>
       <div>
-        <p class="slider-caption">Donec sollicitudin molestie malesuada.</p>
+        <div class="caption">
+          <p>O verão chegou com novidades</p>
+          <a href="#">VER PRODUTOS</a>
+        </div>
         <img class="img-fluid" src="./assets/images/image.png" alt="" />
       </div>
       <div>
-        <p class="slider-caption">Donec sollicitudin molestie malesuada.</p>
+        <div class="caption">
+          <p>O verão chegou com novidades</p>
+          <a href="#">VER PRODUTOS</a>
+        </div>
         <img class="img-fluid" src="./assets/images/image.png" alt="" />
       </div>
       `
@@ -23,6 +32,84 @@ $(document).ready(function() {
       infinite: true,
       mobileFirst: true,
       arrows: false,
-      dots: true
+      dots: true,
+      autoplay: true
+    });
+});
+
+$(document).ready(function() {
+  $("#products-slider")
+    .append(
+      `
+        <div class="product">
+          <img src="./assets/images/model1.png" />
+          <p>Biquíni cortininha palmeira</p>
+          <h2><span>R$ </span>129,90</h2>
+        </div>
+        <div class="product">
+          <img src="./assets/images/model1.png" />
+          <p>Biquíni cortininha palmeira</p>
+          <h2><span>R$ </span>129,90</h2>
+        </div>
+        <div class="product">
+          <img src="./assets/images/model1.png" />
+          <p>Biquíni cortininha palmeira</p>
+          <h2><span>R$ </span>129,90</h2>
+        </div>
+        <div class="product">
+          <img src="./assets/images/model1.png" />
+          <p>Biquíni cortininha palmeira</p>
+          <h2><span>R$ </span>129,90</h2>
+        </div>
+        <div class="product">
+          <img src="./assets/images/model1.png" />
+          <p>Biquíni cortininha palmeira</p>
+          <h2><span>R$ </span>129,90</h2>
+        </div>
+        <div class="product">
+          <img src="./assets/images/model1.png" />
+          <p>Biquíni cortininha palmeira</p>
+          <h2><span>R$ </span>129,90</h2>
+        </div>
+      `
+    )
+    .slick({
+      accessibility: true,
+      infinite: true,
+      mobileFirst: true,
+      dots: true,
+      prevArrow: `<button type="button" class="slick-prev"><</button>`,
+      nextArrow: `<button type="button" class="slick-next">></button>`,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
 });
