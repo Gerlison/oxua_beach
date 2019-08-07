21553d58d44de58ac46ccabd7c9d0b1dba51544c
Francisco Gerlison <franciscogerlison1@gmail.com>
2019-08-07 07:53:13 -0300

    source added

---
+++ services/source.js
@@ -0,0 +1,171 @@
+const menu = [
+  {
+    label: "BIQUÍNIS",
+    link: "#"
+  },
+  {
+    label: "MAIÔS",
+    link: "#"
+  },
+  {
+    label: "BODIES",
+    link: "#"
+  },
+  {
+    label: "SAÍDAS",
+    link: "#"
+  },
+  {
+    label: "SALE",
+    link: "#"
+  }
+];
+
+const headerCarousel = [
+  {
+    caption: "O verão chegou com novidades",
+    image: "./assets/images/image.png",
+    cta_label: "VER PRODUTOS",
+    cta_link: "#"
+  },
+  {
+    caption: "O verão chegou com novidades",
+    image: "./assets/images/image.png",
+    cta_label: "VER PRODUTOS",
+    cta_link: "#"
+  },
+  {
+    caption: "O verão chegou com novidades",
+    image: "./assets/images/image.png",
+    cta_label: "VER PRODUTOS",
+    cta_link: "#"
+  }
+];
+
+const cards = [
+  {
+    image: "./assets/images/maio1.png"
+  },
+  {
+    image: "./assets/images/maio2.png"
+  },
+  {
+    image: "./assets/images/maio3.png"
+  }
+];
+
+const products = [
+  {
+    image: "./assets/images/model1.png",
+    label: "Biquíni cortininha palmeira",
+    price: "129,90"
+  },
+  {
+    image: "./assets/images/model2.png",
+    label: "Biquíni cortininha palmeira",
+    price: "129,90"
+  },
+  {
+    image: "./assets/images/model3.png",
+    label: "Biquíni cortininha palmeira",
+    price: "129,90"
+  },
+  {
+    image: "./assets/images/model4.png",
+    label: "Biquíni cortininha palmeira",
+    price: "129,90"
+  },
+  {
+    image: "./assets/images/model5.png",
+    label: "Biquíni cortininha palmeira",
+    price: "129,90"
+  }
+];
+
+const footer = [
+  {
+    columns: [
+      {
+        label: "Sobre nós",
+        link: "#"
+      },
+      {
+        label: "Fale Conosco",
+        link: "#"
+      },
+      {
+        label: "Atacado",
+        link: "#"
+      }
+    ]
+  },
+  {
+    columns: [
+      {
+        label: "Dúvidas Frequentes",
+        link: "#"
+      },
+      {
+        label: "Entregas e Prazos",
+        link: "#"
+      },
+      {
+        label: "Trocas e Devoluções",
+        link: "#"
+      },
+      {
+        label: "Política de Privacidade",
+        link: "#"
+      },
+      {
+        label: "Formas de Pagamento",
+        link: "#"
+      },
+      {
+        label: "Tabela de Medidas",
+        link: "#"
+      }
+    ]
+  },
+  {
+    columns: [
+      {
+        label: "Entrar",
+        link: "#"
+      },
+      {
+        label: "Minha Conta",
+        link: "#"
+      },
+      {
+        label: "Meus Pedidos",
+        link: "#"
+      },
+      {
+        label: "Central de Atendimento",
+        link: "#"
+      }
+    ]
+  },
+
+  {
+    columns: [
+      {
+        label: "Biquínis",
+        link: "#"
+      },
+      {
+        label: "Maiôs",
+        link: "#"
+      },
+      {
+        label: "Bodies",
+        link: "#"
+      },
+      {
+        label: "Saídas",
+        link: "#"
+      }
+    ]
+  }
+];
